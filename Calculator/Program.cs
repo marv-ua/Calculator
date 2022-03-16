3a08b16fed8ddf76d1bb64a946715205c85fe659
marv-ua <lg.marvel@gmail.com>
2022-03-16 15:17:43 +0200

    logging in json format

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -75,6 +75,10 @@ namespace CalculatorProgram
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+
+            // Add call to close the JSON writer before return
+            calculator.Finish();
+
             return;
         }
     }
